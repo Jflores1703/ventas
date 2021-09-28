17791e10381f3a6d51538deb713fe70157792c3b
Jflores1703 <90058121+Jflores1703@users.noreply.github.com>
2021-09-27 21:36:53 -0600

    Add files via upload
    Subida de archivos debido a reinicio de repositorios según Ticket 1327339 soporte.github.

---
+++ HolaMundo/HolaMundo/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("El único modo de hacer un gran trabajo es amar lo que haces \n- Steve Jobs");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ventas
+LP3
